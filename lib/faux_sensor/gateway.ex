4670dacf03702272a614ca7c84ba4ad24fca887b
Anonirato <vianajose.97@gmail.com>
2020-01-03 19:03:07 +0000

    update faux sensor para ler do serial

--- lib/faux_sensor/gateway.ex
+++ lib/faux_sensor/gateway.ex
@@ -9,23 +9,24 @@ defmodule FauxSensor.Gateway do
   def init(ip, port) do
     TcpServer.start_link(ip, port)
 
-    Agent.start_link(fn -> %{"count" => 0, "pids" => %{}, "ip" => nil, "port" => nil, "uuid" => nil} end,
+    Agent.start_link(
+      fn -> %{"count" => 0, "pids" => %{}, "ip" => nil, "port" => nil, "uuid" => nil} end,
       name: __MODULE__
     )
 
-    #send_request_create(ip, port)
+    # send_request_create(ip, port)
 
     QrCode.generate(ip, port)
 
     Circuits.UART.start_link(name: Circuits)
-    #TODO: change port
-    Circuits.UART.open(Circuits, "/dev/cu.SLAB_USBtoUART", speed: 115200, active: false)
+    # TODO: change port
+    Circuits.UART.open(Circuits, "/dev/cu.SLAB_USBtoUART", speed: 115_200, active: false)
   end
 
   def add_sensor(pid) do
-    #Logger.info("Add sensor")
+    # Logger.info("Add sensor")
 
-    #id = add_new_pid(uuid, state, pid)
+    # id = add_new_pid(uuid, state, pid)
     Agent.get_and_update(__MODULE__, &add_new_pid(&1, pid))
   end
 
@@ -40,14 +41,15 @@ defmodule FauxSensor.Gateway do
       Agent.get(__MODULE__, fn %{"uuid" => uuid, "pids" => pids} = _state -> {uuid, pids} end)
 
     id = pids[sensor_pid]
-    #Logger.info("Sent to mqtt")
+    # Logger.info("Sent to mqtt")
     Publish.send_data(uuid, id, data)
   end
 
   def send_request_create(ip, port) do
     Application.ensure_all_started(:inets)
 
-    {:ok, params} = Jason.encode(%{onboarding: %{"ip" => ip, "port" => port, "uuid" => Ecto.UUID.generate()}})
+    {:ok, params} =
+      Jason.encode(%{onboarding: %{"ip" => ip, "port" => port, "uuid" => Ecto.UUID.generate()}})
 
     :httpc.request(
       :post,
@@ -64,13 +66,14 @@ defmodule FauxSensor.Gateway do
   def add_new_pid(state, pid) do
     count = state["count"]
     pids = state["pids"]
-    uuid =  state["uuid"]
-    new_pids = Map.put(pids, pid, count) #%{pids | IO.inspect(pid) => count}
+    uuid = state["uuid"]
+    # %{pids | IO.inspect(pid) => count}
+    new_pids = Map.put(pids, pid, count)
     new_count = count + 1
     new_state = %{state | "pids" => new_pids, "count" => new_count}
     Publish.send_sensor_status(uuid, new_count)
     {new_count, new_state}
-    #Agent.update(__MODULE__, fn _state -> new_state end)
-    #count
+    # Agent.update(__MODULE__, fn _state -> new_state end)
+    # count
   end
 end
--- lib/faux_sensor/sensor.ex
+++ lib/faux_sensor/sensor.ex
@@ -4,7 +4,7 @@ defmodule FauxSensor.Sensor do
   alias FauxSensor.Gateway
 
   def start_link() do
-    GenServer.start_link(__MODULE__, [])
+    GenServer.start_link(__MODULE__, %{buffer: "", id: 0})
   end
 
   def init(state) do
@@ -13,7 +13,7 @@ defmodule FauxSensor.Sensor do
   end
 
   def handle_info(:start, state) do
-    #Logger.info("start")
+    # Logger.info("start")
 
     if Gateway.add_sensor(self()) < 0 do
       Process.send_after(self(), :start, 1_000)
@@ -25,10 +25,44 @@ defmodule FauxSensor.Sensor do
   end
 
   def handle_info(:wake_up, state) do
-    #Logger.info("wake up")
+    # Logger.info("wake up")
     wake_up()
 
     {:noreply, state}
+  end
+
+  def handle_info(:data, state) do
+    {:ok, message} = Circuits.UART.read(Circuits, 60000)
+    IO.inspect(message)
+    buffer = state.buffer
+    split_array = String.split(buffer <> message, "}")
+
+    if length(split_array) > 1 do
+      droped = Enum.drop(split_array, -1)
+
+      Enum.each(
+        droped,
+        fn x -> Gateway.send_to_mqtt(self(), parse(x <> "}")) end
+      )
+      new_state = %{buffer: List.last(split_array)}
+      {:noreply, Map.merge(state, new_state)}
+    else
+      {:noreply, state}
+    end
+  end
+
+  defp parse(json) do
+    {:ok, data} = Jason.decode(json)
+    split_array = String.split(data["reading"], "&")
+    Jason.encode(%{
+      :date => Timex.now(),
+      :temperature => split_array[0],
+      :pressure => split_array[1],
+      :light => split_array[2],
+      :noise => split_array[3],
+      :humidity => split_array[0], #TODO: build correct json on sensor
+      :accelerometer => split_array[1],
+    }, encode: :unicode_safe)
   end
 
   def wake_up do
@@ -37,25 +71,26 @@ defmodule FauxSensor.Sensor do
   end
 
   def reading do
-    {:ok, read} = data()
-    Gateway.send_to_mqtt(self(), read)
+    send(self(), :data)
+    #{:ok, read} = data()
+    #Gateway.send_to_mqtt(self(), read)
   end
 
   # Generate fake data
-  def data do
-    {:ok, message} = Circuits.UART.read(Circuits, 60000)
-    IO.inspect message
-    
-    Jason.encode(%{
-      :date => Timex.now(),
-      :temperature => random(),
-      :pressure => random(),
-      :light => random(),
-      :noise => random(),
-      :humidity => random(),
-      :accelerometer => 0
-    }, encode: :unicode_safe)
-  end
+  # def data do
+  #   {:ok, message} = Circuits.UART.read(Circuits, 60000)
+  #   IO.inspect message
+  #   
+  #   Jason.encode(%{
+  #     :date => Timex.now(),
+  #     :temperature => random(),
+  #     :pressure => random(),
+  #     :light => random(),
+  #     :noise => random(),
+  #     :humidity => random(),
+  #     :accelerometer => 0
+  #   }, encode: :unicode_safe)
+  # end
 
   def start_transmite do
     Process.send_after(self(), :wake_up, 5_000)
